cd8a256005d2d51d26e8884c66ef8b326cb2c65f
TheEvolutionZ <simonalvarezbedoya2@gmail.com>
2026-08-28 13:38:50 -0500

    style: refine responsive content layout

--- src/components/layout/LayoutPrincipal.jsx
+++ src/components/layout/LayoutPrincipal.jsx
@@ -8,10 +8,10 @@ function LayoutPrincipal({ usuario, onLogout }) {
   return (
     <div className="min-h-screen text-slate-100">
       <FondoAlquimico />
-      <BarraLateral />
-      <div className="min-h-screen lg:pl-64">
+      <BarraLateral usuario={usuario} />
+      <div className="min-h-screen lg:pl-[17rem]">
         <BarraSuperior usuario={usuario} onLogout={onLogout} />
-        <main className="mx-auto w-full max-w-[1500px] px-4 pt-8 pb-28 sm:px-6 lg:px-8 lg:pt-10 lg:pb-12">
+        <main className="mx-auto w-full max-w-[1440px] px-4 pt-7 pb-28 sm:px-6 sm:pt-9 lg:px-8 lg:pt-10 lg:pb-12">
           <Outlet />
         </main>
       </div>
